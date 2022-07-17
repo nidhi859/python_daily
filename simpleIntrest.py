c274367784ab60baf527a5fd15296f1a54098f77
nidhi859 <54396768+nidhi859@users.noreply.github.com>
2022-07-17 16:11:48 +0530

    Add files via upload

---
+++ simpleIntrest.py
@@ -0,0 +1,5 @@
+if __name__ == "__main__":
+    p = int(input("enter vale for principal ",))
+    t = int(input("enter vale for time ",))
+    r = int(input("enter vale for rate ",))
+    print(f"simple intrest is {(p*t*r)/100}")
